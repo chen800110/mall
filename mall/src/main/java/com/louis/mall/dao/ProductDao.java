440ce6df1dd373f18e91a5dbdbbadacb43c279db
chen800110 <hilouis2000@gmail.com>
2024-10-12 02:40:34 +0800

    Delete mall directory

--- mall/src/main/java/com/louis/mall/dao/ProductDao.java
+++
@@ -1,8 +0,0 @@
-package com.louis.mall.dao;
-
-import com.louis.mall.model.Product;
-
-public interface ProductDao {
-
-    Product getProductById(Integer productId);
-}
--- mall/src/main/java/com/louis/mall/service/ProductService.java
+++
@@ -1,7 +0,0 @@
-package com.louis.mall.service;
-
-import com.louis.mall.model.Product;
-
-public interface ProductService {
-    Product getProductById(Integer productId);
-}
